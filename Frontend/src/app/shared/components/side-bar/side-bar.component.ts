899f7741b9b45d7cb92cd698d8d58e7b86f87479
Bailey <sabour047@gmail.com>
2026-01-23 14:05:21 -0600

    moved welcome message to side bar

--- Frontend/src/app/shared/components/side-bar/side-bar.component.ts
+++ Frontend/src/app/shared/components/side-bar/side-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth/auth.service';
 
 @Component({
   selector: 'app-side-bar',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrl: './side-bar.component.css'
 })
 export class SideBarComponent {
+  isAuthenticated: boolean = false;
+  username: string | null = null;
 
+  constructor(private authService: AuthService) {
+    this.username = localStorage.getItem('username');
+  }
+
+  ngOnInit(): void {
+    this.authService.isAuthenticated().subscribe(isAuthenticated => {
+      this.isAuthenticated = isAuthenticated;
+    });
+  }
 }
